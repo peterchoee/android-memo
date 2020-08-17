47f4f8db7522981ca0f4559a99097c610248ce64
choedeb <choedeb@gmail.com>
2020-08-18 06:31:44 +0900

    fix. RxExtension 함수에서 RxJava2 -> RxJava3 변환하도록 수정

--- common/src/main/java/io/choedeb/android/memo/common/RxExtension.kt
+++ common/src/main/java/io/choedeb/android/memo/common/RxExtension.kt
@@ -1,14 +1,19 @@
 package io.choedeb.android.memo.common
 
+import hu.akarnokd.rxjava3.bridge.RxJavaBridge
+import io.reactivex.Completable
+import io.reactivex.Single
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-fun <T> Single<T>.ioSchedulerWithMainThread(): Single<T> {
-    return this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+fun <T> Single<T>.ioWithMainThread(): io.reactivex.rxjava3.core.Single<T> {
+    return RxJavaBridge.toV3Single(this)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
 }
 
-fun Completable.ioSchedulerWithMainThread(): Completable {
-    return this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+fun Completable.ioWithMainThread(): io.reactivex.rxjava3.core.Completable {
+    return RxJavaBridge.toV3Completable(this)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
 }
--- data/src/main/java/io/choedeb/android/memo/data/repository/MemoRepositoryImpl.kt
+++ data/src/main/java/io/choedeb/android/memo/data/repository/MemoRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package io.choedeb.android.memo.data.repository
 
-import hu.akarnokd.rxjava3.bridge.RxJavaBridge
-import io.choedeb.android.memo.common.ioSchedulerWithMainThread
+import io.choedeb.android.memo.common.ioWithMainThread
 import io.choedeb.android.memo.data.local.AppDatabaseDao
 import io.choedeb.android.memo.data.mapper.*
 import io.choedeb.android.memo.domain.entity.DomainEntity
@@ -16,28 +15,26 @@ class MemoRepositoryImpl(
 ) : MemoRepository {
 
     override fun getMemos(): Single<List<DomainEntity.MemoAndImages>> {
-        return RxJavaBridge.toV3Single(appDatabaseDao.selectMemos().map {
+        return appDatabaseDao.selectMemos().map {
             it.map { data ->
                 DomainEntity.MemoAndImages(memoMapper.toDomainEntity(data.memo), imagesMapper.toDomainEntity(data.images))
             }
-        }).ioSchedulerWithMainThread()
+        }.ioWithMainThread()
     }
 
     override fun getMemo(memoId: Long): Single<DomainEntity.MemoAndImages> {
-        return RxJavaBridge.toV3Single(appDatabaseDao.selectMemo(memoId).map { data ->
+        return appDatabaseDao.selectMemo(memoId).map { data ->
             DomainEntity.MemoAndImages(memoMapper.toDomainEntity(data.memo), imagesMapper.toDomainEntity(data.images))
-        }).ioSchedulerWithMainThread()
+        }.ioWithMainThread()
     }
 
     override fun deleteMemo(memo: DomainEntity.Memo): Completable {
-        return RxJavaBridge.toV3Completable(appDatabaseDao.deleteMemo(memoMapper.toDataEntity(memo)))
-            .ioSchedulerWithMainThread()
+        return appDatabaseDao.deleteMemo(memoMapper.toDataEntity(memo)).ioWithMainThread()
     }
 
     override fun setMemoAndImages(memo: DomainEntity.Memo, images: List<DomainEntity.Image>): Completable {
-        return RxJavaBridge.toV3Completable(
-            io.reactivex.Completable.fromAction {
-                appDatabaseDao.insertMemoAndImages(memoMapper.toDataEntity(memo), imagesMapper.toDataEntity(images))
-            }).ioSchedulerWithMainThread()
+        return io.reactivex.Completable.fromAction {
+            appDatabaseDao.insertMemoAndImages(memoMapper.toDataEntity(memo), imagesMapper.toDataEntity(images))
+        }.ioWithMainThread()
     }
 }
